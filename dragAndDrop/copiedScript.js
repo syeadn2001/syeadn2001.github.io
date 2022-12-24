704a272a40dd54aa5df5bb3e8e28cc0a8b0cf1f2
Adnaan Syed <adnaan.syed01@gmail.com>
2022-12-24 17:51:14 -0500

    Finished Drag and Drop

---
+++ dragAndDrop/copiedScript.js
@@ -0,0 +1,45 @@
+
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 900,
+    autoCenter: true,
+    backgroundColor: '#ffffcc',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('beach', 'assets/beachBg.png');
+    this.load.image('crab', 'assets/crab.png');
+    this.load.image('seagull', 'assets/seagull.png');
+    this.load.image('castle', 'assets/castle.png');
+}
+
+function create() {
+
+    this.add.image(400, 300, 'beach');
+
+    var crab = this.add.sprite(100, 850, 'crab');
+    var seagull = this.add.sprite(300, 800, 'seagull');
+    var castle = this.add.sprite(600, 850, 'castle');
+
+    crab.setInteractive();
+    seagull.setInteractive();
+    castle.setInteractive();
+
+    this.input.setDraggable(crab);
+    this.input.setDraggable(seagull);
+    this.input.setDraggable(castle);
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+
+    });
+}
---
+++ dragAndDrop/script.js
@@ -0,0 +1,46 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 700,
+    autoCenter: true,
+    backgroundColor: '#ffffcc',
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('forest', 'assets/foresBackground.jpeg');
+    this.load.image('city', 'assets/cityBackground.jpeg');
+    this.load.image('space', 'assets/spaceBackground.jpeg');
+    this.load.image('mario', 'assets/mario.png');
+    this.load.image('yoshi', 'assets/yoshi.png');
+    this.load.image('toad', 'assets/toad.png');
+
+}
+
+function create() {
+
+    this.add.image(400, 300, 'forest');
+
+    var mario = this.add.sprite(100, 660, 'mario');
+    var yoshi = this.add.sprite(300, 700, 'yoshi');
+    var toad = this.add.sprite(600, 690, 'toad');
+
+    mario.setInteractive();
+    yoshi.setInteractive();
+    toad.setInteractive();
+
+    this.input.setDraggable(mario);
+    this.input.setDraggable(yoshi);
+    this.input.setDraggable(toad);
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+
+    });
+}

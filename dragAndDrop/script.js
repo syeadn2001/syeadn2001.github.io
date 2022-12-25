50bbe091597e7c6fb0d80013ee5080eb0c648de3
Adnaan Syed <adnaan.syed01@gmail.com>
2022-12-25 12:45:42 -0500

    f

--- dragAndDrop/script.js
+++ dragAndDrop/script.js
@@ -3,7 +3,7 @@ var config = {
     width: 800,
     height: 700,
     autoCenter: true,
-    backgroundColor: '#ffffcc',
+    backgroundColor: '#000000',
     scene: {
         preload: preload,
         create: create
@@ -15,7 +15,7 @@ var game = new Phaser.Game(config);
 function preload() {
     this.load.image('forest', 'assets/forestBackground.jpeg');
     this.load.image('city', 'assets/cityBackground.jpeg');
-    this.load.image('space', 'assets/spaceBackground.jpeg');
+    this.load.image('space', 'assets/spaceBackground.jpg');
     this.load.image('mario', 'assets/mario.png');
     this.load.image('yoshi', 'assets/yoshi.png');
     this.load.image('toad', 'assets/toad.png');
@@ -30,9 +30,9 @@ function create() {
     var yoshi = this.add.sprite(300, 700, 'yoshi');
     var toad = this.add.sprite(600, 690, 'toad');
 
-    mario.scale.setTo(0.3, 0.3);
-    yoshi.scale.setTo(0.3, 0.3);
-    toad.scale.setTo(0.3, 0.3);
+    this.mario.scale.setTo(0.3, 0.3);
+    this.yoshi.scale.setTo(0.3, 0.3);
+    this.toad.scale.setTo(0.3, 0.3);
 
 
     mario.setInteractive();
@@ -48,4 +48,8 @@ function create() {
         gameObject.y = dragY;
 
     });
+}
+
+function changeToSpace() {
+
 }
